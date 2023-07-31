67a262e5bf821b06217ef0d847b51f4465c76d33
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-07-31 16:37:02 +0900

    Add: idによるtodolist削除機能追加

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,9 +1,17 @@
 import styles from './TodoItem.module.scss';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from "../redux/todosReducer";
 
 const TodoItem = () => {
-    
     const todos = useSelector((state) => state.todos);
+    const dispatch = useDispatch();
+
+    const handleDelete = (id) => {
+        dispatch(
+            deleteTodo(id)
+        )
+    }
 
     return (
         <>
@@ -15,7 +23,7 @@ const TodoItem = () => {
                             <p>{todo.text}</p>
                             <div>
                                 <button>完了</button>
-                                <button>削除</button>
+                                <button onClick={()=>handleDelete(todo.id)}>削除</button>
                             </div>
                         </li>
                     )
