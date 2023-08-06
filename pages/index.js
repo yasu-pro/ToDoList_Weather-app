a1dbd5d767b6c32c62c027008c3afc77f52966c5
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-06 10:49:07 +0900

    Ref:TodoListにtodosというpropsを持たせるように変更

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Layout from "../components/layout";
 import AddTodoForm from "../components/AddTodoForm"
-import TodoItem from "../components/TodoItem";
 import TodoList from "../components/TodoList";
+import { useSelector } from "react-redux";
 
-export default function Home({allTodoListData}) {
+export default function Home() {
+    const allTodoListData = useSelector((store) => store.todos.listData);
+    const listData = allTodoListData ? Object.values(allTodoListData) : [];
     return (
         <Layout>
             <div>
-                <TodoList>
-                    <TodoItem />
-                </TodoList>
+                <TodoList todos={listData} />
                 <AddTodoForm />
             </div>
         </Layout>
