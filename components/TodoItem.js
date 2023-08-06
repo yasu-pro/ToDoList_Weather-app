aa0db6586e3b26941c0554c7caaf4ea7024fcff8
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-06 11:00:03 +0900

    Fix/Ref: props受け取りによるコードの修正/改行調整

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,23 +1,18 @@
 import styles from './TodoItem.module.scss';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { format } from "date-fns"
 import { deleteTodo, completeTodo, changeDueDate} from "../redux/todosReducer";
 import ReactDatePicker from "./DatePicker";
 
-const TodoItem = () => {
-    const todos = useSelector((state) => state.todos);
+const TodoItem = ({ todo }) => {
     const dispatch = useDispatch();
 
     const handleDelete = (id) => {
-        dispatch(
-            deleteTodo(id)
-        )
+        dispatch(deleteTodo(id))
     }
 
     const handleComplete = (id) => {
-        dispatch(
-            completeTodo(id)
-        )
+        dispatch(completeTodo(id))
     }
 
     const handleDateChange = (id, date) => {
@@ -26,25 +21,17 @@ const TodoItem = () => {
     };
 
     return (
-        <>
-            {
-                todos.map((todo) => {
-                    return(
-                        <li key={todo.id} className={styles.li}>
-                            {todo.completed === true ? <span>✅</span> : ''}
-                            <p>{todo.text}</p>
-                            <div>
-                                <button onClick={()=>handleComplete(todo.id)}>完了</button>
-                                <button onClick={()=>handleDelete(todo.id)}>削除</button>
-                            </div>
-                            <span>期日:{todo.dueDate}</span>
-                            <ReactDatePicker isChangeDueDate={true} onDateChange={(date) => handleDateChange(todo.id, date)}  />
-                        </li>
-                    )
-                })
-            }
-        </>
-    )
+        <li key={todo.id} className={styles.li}>
+            {todo.completed === true ? <span>✅</span> : '未完了'}
+            <p>{todo.text}</p>
+            <span>期日:{todo.dueDate}</span>
+            <div>
+                <button onClick={() => handleComplete(todo.id)}>完了</button>
+                <button onClick={() => handleDelete(todo.id)}>削除</button>
+            </div>
+            <ReactDatePicker isChangeDueDate={true} onDateChange={(date) => handleDateChange(todo.id, date)}  />
+        </li>
+    );
 }
 
 export default TodoItem;
