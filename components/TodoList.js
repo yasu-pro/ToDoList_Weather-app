bfde711954c2a433b07d7b2e7c897f908007759a
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-10 17:47:58 +0900

    Add: ソート機能を追加(SortOptions 関数)

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,12 +1,16 @@
 
 import styles  from "./TodoList.module.scss";
 import TodoItem from "./TodoItem";
+import SortOptions from "./SortOptions";
 
 const TodoList = ({ todos }) => {
+
+    const sortedTodos = SortOptions({todos})
+
     return (
         <ul className={styles.list}>
-            {todos.length > 0 ? (
-                todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+            {sortedTodos.length > 0 ? (
+                sortedTodos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
             ) : (
                 <p>Todoを追加してください。</p>
             )}
