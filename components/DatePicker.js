af863ec8a6724c3e36d27e3a10fc96e95146827a
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-15 11:53:11 +0900

    Add: DatePicker用のcssファイル追加

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import styles from '../styles/DatePicker.module.css';
 
 const ReactDatePicker = ({ selected, onDateChange }) => {
 
@@ -14,6 +15,7 @@ const ReactDatePicker = ({ selected, onDateChange }) => {
       dateFormat="yyyy年MM月dd日"
       selected={selected}
       onChange={(date) => handleDateChange(date)}
+      className={styles["custom-datepicker-input"]}
     />
   );
 };
