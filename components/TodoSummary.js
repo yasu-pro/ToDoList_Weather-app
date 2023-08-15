964f09eeb0c2bc55b47b5209652fe2c7f343858e
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-16 08:57:40 +0900

    Add: TodoSummaryコンポーネントclass付与

--- components/TodoSummary.js
+++ components/TodoSummary.js
@@ -1,4 +1,6 @@
 import React from "react";
+import customBoxStyles from '../styles/customBox.module.css';
+import customSummaryStyles from '../styles/customSummary.module.css';
 
 const TodoSummary = ( {todos} ) => {
     const totalTodo = todos.length > 0 ? todos.length : 0;
@@ -13,26 +15,31 @@ const TodoSummary = ( {todos} ) => {
     }
 
     return(
-        <table style={{ border: "1px solid blue" }}>
-            <tbody>
-                <tr>
-                    <th>現在のタスク総数</th>
-                    <td>{totalTodo} 個</td>
-                </tr>
-                <tr>
-                    <th>残りのタスク</th>
-                    <td>{uncompleted} 個</td>
-                </tr>
-                <tr>
-                    <th>完了済のタスク</th>
-                    <td>{completedTodo} 個</td>
-                </tr>
-                <tr>
-                    <th>完遂率</th>
-                    <td>{calculateCompletionRate()} %</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className={`${customBoxStyles.customBox} pt-5 pr-5 pb-5 pl-5`}>
+            <p className="text-lg font-semibold">タスクの状況</p>
+            <div className="mt-3 border-t-2 border-rgb-243-244-246">
+                <table className={customSummaryStyles.customTable}>
+                    <tbody>
+                        <tr className={customSummaryStyles.customTr}>
+                            <th className={customSummaryStyles.customTh}>現在のタスク総数</th>
+                            <td className={customSummaryStyles.customTd}>{totalTodo} 個</td>
+                        </tr>
+                        <tr className={customSummaryStyles.customTr}>
+                            <th className={customSummaryStyles.customTh}>残りのタスク</th>
+                            <td className={customSummaryStyles.customTd}>{uncompleted} 個</td>
+                        </tr>
+                        <tr className={customSummaryStyles.customTr}>
+                            <th className={customSummaryStyles.customTh}>完了済のタスク</th>
+                            <td className={customSummaryStyles.customTd}>{completedTodo} 個</td>
+                        </tr>
+                        <tr className={customSummaryStyles.customTr}>
+                            <th className={customSummaryStyles.customTh}>完遂率</th>
+                            <td className={customSummaryStyles.customTd}>{calculateCompletionRate()} %</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>  
+        </div>
     )
 }
 
