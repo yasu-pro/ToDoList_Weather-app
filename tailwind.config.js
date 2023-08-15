a026cf2e59237984e178708c403b56d783eaeaf9
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-15 11:52:28 +0900

    Add: テーマカラーのカスタムグレーを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,11 @@ module.exports = {
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
     ],
+    theme: {
+      extend: {
+        colors: {
+          customGray: 'rgb(204, 204, 204)',
+        },
+      },
+    },
   };
