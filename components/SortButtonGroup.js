22507724bf25dab30d48bb1981562f97dd48aded
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-16 17:54:11 +0900

    Fix: ソート用のボタンのcssを変更

--- components/SortButtonGroup.js
+++ components/SortButtonGroup.js
@@ -21,12 +21,12 @@ const SortButtonGroup = () => {
     return (
         <div className='flex items-center'>
             <span className='flex items-center'>
-                <select className={`${styles.cusotmSortButton} pt-2 pr-2 pb-2 pl-2`} defaultValue={sortOrder} onChange={handleSortOrderChange}>
+                <select className={`${styles.cusotmSortButton} pt-2 pr-2 pb-2 pl-4`} defaultValue={sortOrder} onChange={handleSortOrderChange}>
                     <option value="asc">昇順</option>
                     <option value="dsc">降順</option>
                 </select>
 
-                <select className={`${styles.cusotmSortButton} pt-2 pr-3 pb-2 pl-2`} onChange={handleChangeSortBy}>
+                <select className={`${styles.cusotmSortButton} pt-2 pr-3 pb-2 pl-4`} onChange={handleChangeSortBy}>
                     <option value="addOrder">追加順</option>
                     <option value="dueDate">期日順</option>
                     <option value="priority">優先度順</option>
