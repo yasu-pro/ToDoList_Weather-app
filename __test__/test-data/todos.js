46ba37ac6472e3d48a110e54f83e819dd776d9e5
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-25 21:23:04 +0900

    Add: テストデータをまとめて定義するファイルを作成

---
+++ __test__/test-data/todos.js
@@ -0,0 +1,28 @@
+export const initialState = {
+    todos: {
+        listData: {
+            c3iuwvt: {
+            id: 'c3iuwvt',
+            isEditFormVisible: false,
+            text: 'test',
+            completed: false,
+            dueDate: '2023年08月24日',
+            priority: '2',
+            addOrder: 1692860835709,
+            sortBy: 'addOrder',
+            sortOrder: 'asc'
+            },
+            hnn4qj0: {
+            id: 'hnn4qj0',
+            isEditFormVisible: false,
+            text: 'test2',
+            completed: false,
+            dueDate: '2023年08月24日',
+            priority: '3',
+            addOrder: 1692860842939,
+            sortBy: 'addOrder',
+            sortOrder: 'asc'
+            }
+        }
+    }
+  };
