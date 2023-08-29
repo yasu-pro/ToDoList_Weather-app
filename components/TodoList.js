5f8472c782dafe709037d75d81f39219a836e5ff
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-29 10:15:27 +0900

    Ref: 初期値を変更によるprops,引数を変更

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,15 +1,12 @@
 import React from "react";
 import TodoItem from "./TodoItem";
-import SortOptions from "./SortOptions";
 
-const TodoList = ({ todos }) => {
-
-    const sortedTodos = SortOptions({todos})
+const TodoList = ({ sortedListData }) => {
 
     return (
         <ul className="mx-auto">
-            {sortedTodos.length > 0 ? (
-                sortedTodos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
+            {sortedListData.length > 0 ? (
+                sortedListData.map((todo) => <TodoItem key={todo.id} todo={todo} />)
             ) : (
                 <p>Todoを追加してください。</p>
             )}
--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,17 @@ import TodoList from "../components/TodoList";
 import TodoSummary from "../components/TodoSummary";
 import SortButtonGroup from "../components/SortButtonGroup";
 import ClearAllButton from "../components/ClearAllButton";
+import SortOptions from "../components/SortOptions";
 
 export default function Home() {
-    const allTodoListData = useSelector((store) => store.todos.listData);
-    const listData = allTodoListData ? Object.values(allTodoListData) : [];
+    const allTodoData = useSelector(( store ) => store.todos.data);
+    const sortedListData = SortOptions({ allTodoData });
+
     return (
         <Layout>
             <div className="pt-3 pr-3 pb-3 pl-3 md:pt-12 md:pr-4 md:pb-12 md:pl-4">
                 <div className="pt-3 pr-0 pb-3 pl-0 md:pt-5 md:pr-5 md:pb-5 md:pl-5">
-                    <TodoList todos={listData} />
+                    <TodoList sortedListData={ sortedListData } />
                 </div>
 
                 <div className="grid gap-5 md:grid-cols-2 md:gap-10">
@@ -23,13 +25,13 @@ export default function Home() {
 
                     <div className="order-1 md:order-2 pt-5 border-t border-rgba-[209,213,219] md:pt-0 md:border-t-0">
                         <div>
-                            <SortButtonGroup todos={listData}/>
+                            <SortButtonGroup />
                             <div className="mt-5">
                                 <ClearAllButton />
                             </div>
                         </div>
                         <div className="mt-0 md:mt-7">
-                            <TodoSummary todos={listData}/>
+                            <TodoSummary sortedListData={ sortedListData }/>
                         </div>
                     </div>
                 </div>
