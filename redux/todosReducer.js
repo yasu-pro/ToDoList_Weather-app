37148e8b1f935dc36afd3a204c6cfc4f5deef547
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-08-29 10:13:22 +0900

    Ref: storeの初期値を変更

--- redux/todosReducer.js
+++ redux/todosReducer.js
@@ -1,6 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { listData: {} };
+const initialState = {
+  data: {
+    optionData: {
+      sortBy: "addOrder",
+      sortOrder: "asc",
+    },
+    ListData: {},
+  }
+};
 
 const todosReducer = createSlice({
   name: 'todos',
@@ -8,47 +16,42 @@ const todosReducer = createSlice({
   reducers: {
     addTodo: (state, action) => {
       const id = action.payload.id;
-      state.listData[id] = { ...action.payload, id};
+      state.data.ListData[id] = { ...action.payload, id};
     },
 
     deleteTodo: (state, action) => {
       const id = action.payload;
-      delete state.listData[id];
+      delete state.data.ListData[id];
     },
 
     completeTodo: (state, action) => {
       const {id, isComplete } = action.payload;
-      state.listData[id].completed = isComplete;
+      state.data.ListData[id].completed = isComplete;
     },
 
     editTodoAction: (state, action) => {
       const id = action.payload.id;
-      state.listData[id] = { ...action.payload};
+      state.data.ListData[id] = { ...action.payload};
     },
 
     showEditForm: (state, action) => {
       const {id, isEditFormVisible} = action.payload;
-      state.listData[id].isEditFormVisible = isEditFormVisible;
+      state.data.ListData[id].isEditFormVisible = isEditFormVisible;
     },
 
     changeSortBy: (state, action) => {
       const sortBy = action.payload;
-      const todosArray = Object.values(state.listData);
-      todosArray.map((todo) => {
-        todo.sortBy = sortBy;
-      })
+      state.data.optionData.sortBy = sortBy;
+
     },
 
     changeSortOrder: (state, action) => {
       const sortOrder = action.payload;
-      const todosArray = Object.values(state.listData);
-      todosArray.map((todo) => {
-        todo.sortOrder = sortOrder;
-      })
+      state.data.optionData.sortOrder = sortOrder;
     },
 
     clearAllTodos: (state) => {
-      state.listData = {}
+      state.data.ListData = {}
     }
   },
 });
