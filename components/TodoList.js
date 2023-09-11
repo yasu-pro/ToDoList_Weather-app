e21036d228434b047fcb533dff3978ba16c01400
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-09-11 17:51:19 +0900

    Ref: TodoListインポート順変更,tailwind修正

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,16 +1,16 @@
 import React from "react";
-import TodoItem from "./TodoItem";
 import SortOptions from '../components/SortOptions';
+import TodoItem from "./TodoItem";
 
-const TodoList = ( allTodoData ) => {
+const TodoList = ({ allTodoData }) => {
     const sortedListData = SortOptions( allTodoData );
 
     return (
-        <ul className="mx-auto">
+        <ul className="mx-auto pt-3 pr-0 pb-3 pl-0 md:pt-5 md:pr-5 md:pb-5 md:pl-5">
             {sortedListData.length > 0 ? (
                 sortedListData.map((todo) => <TodoItem key={todo.id} todo={todo} />)
             ) : (
-                <p>Todoを追加してください。</p>
+                <p className="text-center text-lg">Todoを追加してください。</p>
             )}
         </ul>
     )
