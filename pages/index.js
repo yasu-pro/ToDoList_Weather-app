d3c64f044e5b20098acd6ff27d052f9b0a99c951
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-09-11 17:36:06 +0900

    Ref: index.jsのインポート順、tailwind修正

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Layout from "../components/layout";
+// Component Section
 import AddTodoForm from "../components/AddTodoForm"
+import ClearAllButton from "../components/ClearAllButton";
+import Layout from "../components/layout";
+import SortButtonGroup from "../components/SortButtonGroup";
 import TodoList from "../components/TodoList";
 import TodoSummary from "../components/TodoSummary";
-import SortButtonGroup from "../components/SortButtonGroup";
-import ClearAllButton from "../components/ClearAllButton";
 
 export default function Home() {
     const allTodoData = useSelector(( store ) => store.todos.data);
 
     return (
         <Layout>
-            <div className="pt-3 pr-3 pb-3 pl-3 md:pt-12 md:pr-4 md:pb-12 md:pl-4">
-                <div className="pt-3 pr-0 pb-3 pl-0 md:pt-5 md:pr-5 md:pb-5 md:pl-5">
-                    <TodoList allTodoData={ allTodoData } />
-                </div>
+            <div className="p-3 md:p-12">
+                <TodoList allTodoData={ allTodoData } />
 
                 <div className="grid gap-5 md:grid-cols-2 md:gap-10">
 
                     <AddTodoForm />
 
                     <div className="order-1 md:order-2 pt-5 border-t border-rgba-[209,213,219] md:pt-0 md:border-t-0">
-                        <div>
-                            <SortButtonGroup />
-                            <div className="mt-5">
-                                <ClearAllButton />
-                            </div>
-                        </div>
-                        <div className="mt-0 md:mt-7">
-                            <TodoSummary allTodoData={ allTodoData }/>
-                        </div>
+                        <SortButtonGroup />
+                        <ClearAllButton />
+                        <TodoSummary allTodoData={ allTodoData }/>
                     </div>
                 </div>
             </div>
