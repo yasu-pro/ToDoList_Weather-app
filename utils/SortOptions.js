17c22db4918bb19a54f97ade60679b2f3ed5bfb2
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-09-12 17:47:36 +0900

    Ref: SortOptions.js

--- utils/SortOptions.js
+++ utils/SortOptions.js
@@ -1,19 +1,17 @@
 import { parse } from "date-fns"
 
-const SortOptions = ( allTodoData ) => {
+const SortOptions = ({ ListData, optionData }) => {
+    const listData = ListData ? Object.values(ListData) : [];
 
-    const listData = allTodoData.ListData ? Object.values(allTodoData.ListData) : [];
-
-    const sortedListData = listData.slice().sort((a, b) => {
-        if (allTodoData.optionData.sortBy === "dueDate") {
-            return allTodoData.optionData.sortOrder === "asc" ? parse(a.dueDate, "yyyy年MM月dd日", new Date()) - parse(b.dueDate, "yyyy年MM月dd日", new Date()):parse(b.dueDate, "yyyy年MM月dd日", new Date()) - parse(a.dueDate, "yyyy年MM月dd日", new Date());
-        } else if (allTodoData.optionData.sortBy === "priority") {
-            return allTodoData.optionData.sortOrder === "asc" ? a.priority - b.priority : b.priority - a.priority;
+    return listData.slice().sort((a, b) => {
+        if (optionData.sortBy === "dueDate") {
+            return optionData.sortOrder === "asc" ? parse(a.dueDate, "yyyy年MM月dd日", new Date()) - parse(b.dueDate, "yyyy年MM月dd日", new Date()):parse(b.dueDate, "yyyy年MM月dd日", new Date()) - parse(a.dueDate, "yyyy年MM月dd日", new Date());
+        } else if (optionData.sortBy === "priority") {
+            return optionData.sortOrder === "asc" ? a.priority - b.priority : b.priority - a.priority;
         } else {
-            return allTodoData.optionData.sortOrder === "asc" ? a.addOrder - b.addOrder : b.addOrder - a.addOrder;
+            return optionData.sortOrder === "asc" ? a.addOrder - b.addOrder : b.addOrder - a.addOrder;
         }
     });
-    return sortedListData;
 }
 
 export default SortOptions;
