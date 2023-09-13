876950bbe8aa388e9a0d420df7078fe799ac6873
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-09-13 16:40:42 +0900

    Ref: ModalEditTodoForm.js

--- components/ModalEditTodoForm.js
+++ components/ModalEditTodoForm.js
@@ -1,15 +1,14 @@
-
 import React, { useState } from "react";
-import { format, parse } from "date-fns"
 import { useDispatch } from 'react-redux';
 import { Button } from "@mui/material";
 import Modal from "react-modal";
-import ReactDatePicker from "./DatePicker";
+import { format, parse } from "date-fns"
 import { editTodoAction, showEditForm } from "../redux/todosReducer";
+import ReactDatePicker from "./DatePicker";
 import customButtonStyles from "../styles/customButton.module.css";
 import customRadioButtonStyles from '../styles/customRadioButton.module.css';
 
-const ModalEditTodoForm = ({todo}) => {
+const ModalEditTodoForm = ({ todo }) => {
 
     const customStyles = {
         content: {
@@ -30,8 +29,8 @@ const ModalEditTodoForm = ({todo}) => {
         },
     };
 
-    const [editedTodo, setEditedTodo] = useState(todo);
     const dispatch = useDispatch();
+    const [editedTodo, setEditedTodo] = useState(todo);
 
     const handleEditComplete = () => {
         const updatedTodo = { ...editedTodo, isEditFormVisible: false };
@@ -43,36 +42,52 @@ const ModalEditTodoForm = ({todo}) => {
         dispatch(showEditForm({ id: todo.id, isEditFormVisible: false }));
     }
 
+    const handleTextChange = (e) => {
+        setEditedTodo({ ...editedTodo, text: e.target.value });
+    }
+
+    const handlePriorityChange = (e) => {
+        const newPriority = e.target.value;
+        setEditedTodo({ ...editedTodo, priority: newPriority });
+    }
+
+    const handleDueDateChange = (date) => {
+        const formattedDate = format(date, "yyyy年MM月dd日");
+        setEditedTodo({ ...editedTodo, dueDate: formattedDate });
+    }
+
     return (
         <Modal isOpen={editedTodo.isEditFormVisible} style={customStyles}>
-            <div className="pt-5 pr-5 pb-5 pl-5">
+            <div className="pt-5 pb-5 pl-5 pr-5">
                 <p className="text-lg font-semibold">編集</p>
 
-                <div className="pr-3 pl-3">
+                <div className="pl-3 pr-3">
                     <div className="pt-3">
                         <p className="flex">
                             内容
-                            <span className="mr-1 ml-1 pt-1 pr-1 pb-1 pl-1 text-xs text-red-600 border border-red-500 rounded-sm">
+                            <span className="pt-1 pb-1 pl-1 pr-1 ml-1 mr-1 text-xs text-red-600 border border-red-500 rounded-sm">
                             必須
                             </span>
                             :
                         </p>
-                        <input className={`w-full pt-1.5 pb-1.5 pr-4 pl-4 text-lg mt-1.5 bg-gray-100 ${editedTodo.text === "" ? "bg-red-100" : "bg-gray-100"}`} value={editedTodo.text} onChange={(e) => setEditedTodo({ ...editedTodo, text: e.target.value })}/>
+                        <input className={`w-full pt-1.5 pb-1.5 pr-4 pl-4 text-lg mt-1.5 bg-gray-100 ${editedTodo.text === "" ? "bg-red-100" : "bg-gray-100"}`}
+                            value={editedTodo.text}
+                            onChange={handleTextChange}/>
                         {editedTodo.text === "" && <span className="text-red-600">文字を入力してください</span>}
                     </div>
 
                     <div className="pt-3">
                         <p>優先度 : </p>
-                        <div className="pt-1 w-36 flex items-center justify-between">
+                        <div className="flex items-center justify-between pt-1 w-36">
                             <div>
-                                <label className="ml-1 flex items-center" htmlFor={`low-${editedTodo.id}`}>
+                                <label className="flex items-center ml-1" htmlFor={`low-${editedTodo.id}`}>
                                     <input
                                         type="radio"
                                         id={`low-${editedTodo.id}`}
                                         name={`editedPriority-${editedTodo.id}`}
                                         value="3"
                                         checked={editedTodo.priority === "3"}
-                                        onChange={(e) => setEditedTodo({...editedTodo, priority: e.target.value})}
+                                        onChange={handlePriorityChange}
                                         className={customRadioButtonStyles.customPriorityRadioButton}
                                     />
                                     <span className={`${customRadioButtonStyles.customRadioButtonIcon} ${editedTodo.priority === "3" ? customRadioButtonStyles.lowIcon : ""}`}></span>
@@ -81,14 +96,14 @@ const ModalEditTodoForm = ({todo}) => {
                             </div>
 
                             <div>
-                                <label className="ml-1 flex items-center" htmlFor={`medium-${editedTodo.id}`}>
+                                <label className="flex items-center ml-1" htmlFor={`medium-${editedTodo.id}`}>
                                     <input
                                         type="radio"
                                         id={`medium-${editedTodo.id}`}
                                         name={`editedPriority-${editedTodo.id}`}
                                         value="2"
                                         checked={editedTodo.priority === "2"}
-                                        onChange={(e) => setEditedTodo({...editedTodo, priority: e.target.value})}
+                                        onChange={handlePriorityChange}
                                         className={customRadioButtonStyles.customPriorityRadioButton}
                                     />
                                     <span className={`${customRadioButtonStyles.customRadioButtonIcon} ${editedTodo.priority === "2" ? customRadioButtonStyles.mediumIcon : ""}`}></span>
@@ -97,14 +112,14 @@ const ModalEditTodoForm = ({todo}) => {
                             </div>
 
                             <div>
-                                <label className="ml-1 flex items-center" htmlFor={`high-${editedTodo.id}`}>
+                                <label className="flex items-center ml-1" htmlFor={`high-${editedTodo.id}`}>
                                     <input
                                         type="radio"
                                         id={`high-${editedTodo.id}`}
                                         name={`editedPriority-${editedTodo.id}`}
                                         value="1"
                                         checked={editedTodo.priority === "1"}
-                                        onChange={(e) => setEditedTodo({...editedTodo, priority: e.target.value})}
+                                        onChange={handlePriorityChange}
                                         className={customRadioButtonStyles.customPriorityRadioButton}
                                     />
                                     <span className={`${customRadioButtonStyles.customRadioButtonIcon} ${editedTodo.priority === "1" ? customRadioButtonStyles.hightIcon : ""}`}></span>
@@ -118,13 +133,13 @@ const ModalEditTodoForm = ({todo}) => {
                         <p>期限 : </p>
                         <ReactDatePicker id="editedDueDate"
                             selected={parse(editedTodo.dueDate, "yyyy年MM月dd日", new Date())}
-                            onDateChange={(date) => setEditedTodo({...editedTodo, dueDate: format(date, "yyyy年MM月dd日")})}
+                            onDateChange={handleDueDateChange}
                         />
                     </div>
                 </div>
             </div>
 
-            <div className="pt-5 pr-5 pb-5 pl-5 flex justify-end border-t border-solid bg-gray-100 overflow-hidden rounded-tl-md rounded-tr-md rounded-bl-lg rounded-br-lg">
+            <div className="flex justify-end pt-5 pb-5 pl-5 pr-5 overflow-hidden bg-gray-100 border-t border-solid rounded-bl-lg rounded-br-lg rounded-tl-md rounded-tr-md">
                 <Button
                     variant="outlined"
                     color="primary"
