8c34c7d26489df5e3b0ca783e68ec013fa9cbb0f
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-09-16 18:26:55 +0900

    Change: _app from .js file to tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from '../redux/store';
+import { store } from '../redux/store';
 import Modal from 'react-modal';
 import '../styles/global.css';
 import '../styles/DatePickerOverrides.css';
 
+// Modalの設定
 Modal.setAppElement('#__next');
 
-function MyApp({ Component, pageProps }) {
+interface MyAppProps {
+  Component: React.ComponentType<any>;
+  pageProps: any;
+}
+
+const MyApp: React.FC<MyAppProps> = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <React.StrictMode>
