94785537e04460c35ee1d10bdd104fb453fd9c94
yasu-pro <cb1-y.s@docomo.ne.jp>
2023-12-21 21:24:45 +0900

    feat: Adjust date calculations using setHours to fetch data for the next 3 days, including today
    This commit updates the code to include data for the next 3 days, ensuring accurate date calculations by using the setHours function.

--- components/weather/ThreeDaysWeather.tsx
+++ components/weather/ThreeDaysWeather.tsx
@@ -52,19 +52,25 @@ const groupedWeatherData = (weatherByToday: DailyData[]) => {
         const today = new Date();
 
         // 今日を含めて明日、明後日までのデータしか取得しない
-        const futureDates = new Date(today);
-        futureDates.setDate(today.getDate() + 2);
+        const futureDateLimit = new Date(today);
+        futureDateLimit.setDate(today.getDate() + 2);
+
+        // 時間部分をゼロに設定
+        const currentDate = new Date(date);
+        const futureDatesCopy = new Date(futureDateLimit);
+        currentDate.setHours(0, 0, 0, 0);
+        futureDatesCopy.setHours(0, 0, 0, 0);
 
         // 今日の日付を含むデータを取得する条件
-        if (date <= futureDates) {
+        if (currentDate <= futureDatesCopy) {
             const precipitationProbability = Math.round(data.pop * 100);
             const roundedPercentage = Math.round(precipitationProbability / 10) * 10;
 
             // apiから必要なデータを新規作成
             groupedData.push({
-                dateString: date.toLocaleDateString("ja-JP", { month: "numeric", day: "numeric" }),
-                weekdayString: `(${date.toLocaleDateString("ja-JP", { weekday: "short" })})`,
-                dateObj: date,
+                dateString: currentDate.toLocaleDateString("ja-JP", { month: "numeric", day: "numeric" }),
+                weekdayString: `(${currentDate.toLocaleDateString("ja-JP", { weekday: "short" })})`,
+                dateObj: currentDate,
                 weatherIcon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
                 weatherDescription: data.weather[0].description,
                 maxTemp: Math.round(data.temp.max - 273.15),
