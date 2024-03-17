720b4e401dafe87634af7deb71458fed798790d9
yasu-pro <cb1-y.s@docomo.ne.jp>
2024-03-17 18:29:52 +0900

    Added @mui/icons-material and replaced react-burger-menu with Material UI Drawer

--- _common/BurgerMenu.tsx
+++ _common/BurgerMenu.tsx
@@ -1,58 +1,24 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { slide as Menu } from "react-burger-menu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faCloudSun, faTableList, faGear, faUser } from "@fortawesome/free-solid-svg-icons";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+// import InboxIcon from "@mui/icons-material/MoveToInbox";
+// import MailIcon from "@mui/icons-material/Mail";
 
-const BurgerMenu = () => {
+export default function TemporaryDrawer() {
     const [isPcScreen, setIsPcScreen] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const styles = {
-        bmBurgerButton: {
-            position: "fixed",
-            width: "36px",
-            height: "30px",
-            left: "36px",
-            top: "36px",
-        },
-        bmBurgerBars: {
-            background: "#373a47",
-        },
-        bmBurgerBarsHover: {
-            background: "#a90000",
-        },
-        bmCrossButton: {
-            height: "24px",
-            width: "24px",
-        },
-        bmCross: {
-            background: "#bdc3c7",
-        },
-        bmMenuWrap: {
-            position: "fixed",
-            height: "100%",
-        },
-        bmMenu: {
-            background: "#373a47",
-            padding: "2.5em 1.5em 0",
-            fontSize: "1.15em",
-        },
-        bmMorphShape: {
-            fill: "#373a47",
-        },
-        bmItemList: {
-            color: "#b8b7ad",
-            padding: "0.8em",
-        },
-        bmItem: {
-            display: "block",
-        },
-        bmOverlay: {
-            background: "rgba(0, 0, 0, 0.3)",
-        },
-    };
+    const [open, setOpen] = React.useState(false);
 
     useEffect(() => {
         // クライアントサイドでのみ実行
@@ -61,35 +27,48 @@ const BurgerMenu = () => {
         }
     }, []);
 
-    const handleStateChange = (state: { isOpen: boolean }) => {
-        setMenuOpen(state.isOpen);
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setOpen(newOpen);
     };
 
-    const closeMenu = () => {
-        setMenuOpen(false);
-    };
+    const DrawerList = (
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+            <List>
+                {["todolist", "weather"].map((text, index) => (
+                    <Link href={`/${text}`}>
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>{index % 2 === 0 ? <FontAwesomeIcon icon={faCloudSun} /> : <FontAwesomeIcon icon={faTableList} />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {["setting", "user"].map((text, index) => (
+                    <Link href={`/${text}`}>
+                        <ListItem key={text} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>{index % 2 === 0 ? <FontAwesomeIcon icon={faGear} /> : <FontAwesomeIcon icon={faUser} />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                ))}
+            </List>
+        </Box>
+    );
 
     return isPcScreen ? (
-        <Menu isOpen={menuOpen} onStateChange={handleStateChange} width={250} customBurgerIcon={<FontAwesomeIcon icon={faBars} />} customCrossIcon={<FontAwesomeIcon icon={faTimes} />} styles={styles}>
-            <ul>
-                <li>
-                    <Link href="/todolist" onClick={closeMenu}>
-                        ToDoList
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/weather" onClick={closeMenu}>
-                        Weather
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/settings" onClick={closeMenu}>
-                        Settings
-                    </Link>
-                </li>
-            </ul>
-        </Menu>
+        <div>
+            <IconButton color="inherit" aria-label="open drawer" onClick={toggleDrawer(true)} edge="start" sx={{ mt: 2, ml: 2, position: "fixed", top: "16px", left: "16px", width: "50px", height: "50px", zIndex: "9999", ...(open && { display: "none" }) }}>
+                <MenuIcon />
+            </IconButton>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+            </Drawer>
+        </div>
     ) : null;
-};
-
-export default BurgerMenu;
+}
